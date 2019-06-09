40a549aed74d6fe2a9078adde284e75f12c9abe4
Sem Schreuder <semschreuder@live.nl>
2019-06-09 14:37:36 +0200

    Interface look update + mover objects

--- semtiplex2.js
+++ semtiplex2.js
@@ -6,40 +6,50 @@ autowatch = 1;
 var beforeMatrix 	= new JitterMatrix(1, "char", 255); 
 var afterMatrix 	= new JitterMatrix(1, "char", 255);
 var outputMatrix	= new JitterMatrix(1, "char", 512);
+var outputMatrix2	= new JitterMatrix(1, "char", 512);
 var split1			= new JitterMatrix(1, "char", 512);
 var split2			= new JitterMatrix(1, "char", 512);
+
+var afterAmount		= 0;
 
 var beforeMult		= new JitterObject("jit.multiplex");
 beforeMult.truncate = 0;
 beforeMult.scan_a	= 1;
-beforeMultscan_b	= 1;
+beforeMult.scan_b	= 1;
 
-/*
-var afterMult1		= new JitterObject("jit.multiplex");
-beforeMult.truncate = 0;
-beforeMult.scan_a	= 1;
-beforeMultscan_b	= 1;
-*/
-
-var cutOff			= new JitterObject("jit.split");
-cutOff.splitdim 	= 1;
-cutOff.splitpoint  	= 512;
-
-// scissors.matrixcalc(inMatrix, [outMatrix1, outMatrix2]);
-
-
-function before(v)
+function after(a)
 {
-	beforeMult.scan_a = v;
+	afterAmount = a;	
+}
+
+function before(b)
+{
+	beforeMult.scan_a = b;
 }
 
 function bang() 
 {
-	
 	var envMatrix = new JitterMatrix("semtiplex");	// defined in max patch
 	beforeMult.matrixcalc([beforeMatrix, envMatrix],outputMatrix);
-	cutOff.matrixcalc(outputMatrix,[split1,split2]);
-	outlet(0,"jit_matrix",split1.name);	
-	outlet(1,"jit_matrix",split2.name);	
 
+	if (afterAmount > 0) 
+	{
+		for (var i=0; i<afterAmount; i++) 
+		{
+			var initName = "afterAmt";
+			var resultName = initName.concat(i);	
+
+			var resultName	= new JitterObject("jit.multiplex");
+			beforeMult.truncate = 0;
+			beforeMult.scan_a	= 1;
+			beforeMult.scan_b	= 1;
+
+			resultName.matrixcalc([outputMatrix,beforeMatrix],outputMatrix2)
+			outlet(0,"jit_matrix",outputMatrix2.name);	
+		}		
+	}
+	else
+	{
+		outlet(0,"jit_matrix",outputMatrix.name);		
+	}
 }
